b2419c8f8252104704ccadee3b432b376b5d5355
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2022-10-25 00:43:31 +0300

    Add: Strong Attack Handler && Confetti Animation
    TODO: Refactor Logic of Attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,8 @@
-const ATTACK_VALUE = 15;
+const jsConfetti = new JSConfetti();
+
+const NORMAL_ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let staticMaxLife = 100;
 let currentMonsterHealth = staticMaxLife;
@@ -6,9 +10,32 @@ let currentPlayerHealth = staticMaxLife;
 
 adjustHealthBars(staticMaxLife);
 
-function normalAttackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
+function attackHandler(attackValue) {
+  const monsterDamage = dealMonsterDamage(attackValue);
+  currentMonsterHealth -= monsterDamage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    });
+    alert('You Won Çido');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    });
+    alert('You Lose Çido');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    });
+    alert('You have a draw Çido!');
+  }
 }
 
-normalAttackBtn.addEventListener('click', normalAttackHandler);
+normalAttackBtn.addEventListener('click', () =>
+  attackHandler(NORMAL_ATTACK_VALUE)
+);
+strongAttackBtn.addEventListener('click', () =>
+  attackHandler(STRONG_ATTACK_VALUE)
+);
